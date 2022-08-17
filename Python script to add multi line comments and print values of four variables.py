f4ddb2561575601459aff0be94f04273d0cf79d0
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-17 22:11:08 +0530

    Add files via upload

---
+++ Python script to add multi line comments and print values of four variables.py
@@ -0,0 +1,9 @@
+"""Python script to add multi line comments
+   print values of four variables
+   Variable should contains any value
+   Created by, Gaurav Kumar
+   Date 08/17/2022"""
+
+x,y,z,w = 12,"Twenty",30,40.5
+
+print(x,y,z,w,sep="\n")
