cbe6737ac400c18358346117f59c6426b77e2a3e
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-17 22:56:13 +0530

    Add files via upload

---
+++ Write a Python script to print 4 values type and id of each variable.py
@@ -0,0 +1,12 @@
+"""Create four variables in a Python script and assign values of different data types to them.
+Write a Python script to print value, its type and id of each variable"""
+x = 44
+y = 34.8
+z = None
+w = "Python Basics"
+
+print(f"Value of x is {x}", f"Type of x is {type(x)}", f"Refrence id of x is {id(x)}", sep="|")
+print(f"Value of y is {y}", f"Type of y is {type(y)}", f"Refrence id of y is {id(y)}", sep="|")
+print(f"Value of z is {z}", f"Type of z is {type(z)}", f"Refrence id of z is {id(z)}", sep="|")
+print(f"Value of w is {w}", f"Type of w is {type(w)}", f"Refrence id of w is {id(w)}", sep="|")
+
