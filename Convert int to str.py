2d2bc7c92912dca5bd417c23aa771a2094f2e70f
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-18 00:42:20 +0530

    Add files via upload
    A python script to convert a number into str type

---
+++ Convert int to str.py
@@ -0,0 +1,9 @@
+#Write a python script to convert a number into str type.
+
+#assiging integer value to variable x
+x = 5
+print(type(x))
+
+#here we will use conversion function as str()
+x = str(x)
+print("Value of x:",x,"Type of x:", type(x))
