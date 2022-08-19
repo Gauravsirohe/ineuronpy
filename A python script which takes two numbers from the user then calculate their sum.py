a0c666ed8e8124f0fe858a254b4892bc6e9c5a91
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-19 16:40:39 +0530

    Add files via upload

---
+++ A python script which takes two numbers from the user then calculate their sum.py
@@ -0,0 +1,8 @@
+"""A python script which takes two numbers from the user, then calculate their sum
+and display the result."""
+print("Enter first number")
+x = int(input())
+print("Enter second number")
+y = int(input())
+
+print(f"Sum is {x+y}")
