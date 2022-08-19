98cdbf60cad751b7780ab7f6966d21aa34ac920d
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-19 17:12:42 +0530

    Add files via upload

---
+++ Python script which takes the radius from the user and display area of a circle.py
@@ -0,0 +1,11 @@
+"""Python script which takes the radius from the user and display area of a circle.
+Area of circle 
+area = π · r2  """
+
+#Calculate radius
+radius = int(input("Radius = "))**2 #Get input from user and then find square root
+#value of π
+pai = 3.14 
+
+#Print the value of area of circle
+print("Area of a circle =",pai*radius)
