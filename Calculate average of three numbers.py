8c8698d6ac224772cb974d2d074bf74262cbb449
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-20 00:19:43 +0530

    Add files via upload

---
+++ Calculate average of three numbers.py
@@ -0,0 +1,11 @@
+#Calculate average of any three numbers
+print("Enter first number")
+x = int(input())
+print("Enter second number")
+y = int(input())
+print("Enter third number")
+z = int(input())
+
+avg = (x+y+z)//3
+
+print(f"Avergae of numbers {x},{y},{z} is", avg)
