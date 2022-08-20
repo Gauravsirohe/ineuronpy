08121387cab712b4782e08f47b3d5c5f20f48feb
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-21 00:00:12 +0530

    Add files via upload

---
+++ Find x power y where values of x and y are given by user.py
@@ -0,0 +1,7 @@
+#Python script to find x power y, where values of x and y are given by user
+
+
+x = int(input())
+y = int(input())
+
+print(x**y)
---
+++ Script to swap data of two variables.py
@@ -0,0 +1,11 @@
+#Script to swap data of two variables
+
+x = input("Enter value for x: ")
+y = input("Enter value for y: ")
+
+x,y = y,x
+
+print("Swap the values of x and y :",f"Value of x is {x} and Value of y is {y}")
+
+
+
