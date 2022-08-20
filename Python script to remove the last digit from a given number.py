6f9bd3c55e001cbbadc3cac9e0a41a4f70a85645
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-21 00:55:20 +0530

    Add files via upload

---
+++ Python script to remove the last digit from a given number.py
@@ -0,0 +1,6 @@
+#Python script to remove the last digit from a given number.
+number = int(input("Enter a number : "))
+
+remove_last_digit = number/10
+
+print("Remove the last digit of %d, now number is %d" %(number,remove_last_digit))
