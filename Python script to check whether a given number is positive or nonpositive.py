643c39ef349926289e2f88b1b64d06f304bada49
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-21 15:33:17 +0530

    Add files via upload

---
+++ Python script to check whether a given number is positive or nonpositive.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number : "))
+
+if number > 0:
+    print(f"Given number {number} is positive")
+if number <= 0:
+    print(f"Given number {number} is non-positive")
+print()
