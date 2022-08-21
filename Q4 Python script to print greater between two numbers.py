0e0b9913a10a2514de4b5b2d27fb881a4143ddb5
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-21 16:14:21 +0530

    Add files via upload

---
+++ Q4 Python script to print greater between two numbers.py
@@ -0,0 +1,11 @@
+n1 = int(input("Enter first number : "))
+n2 = int(input("Enter secondnumber : "))
+
+if n1 > n2:
+    print(f"{n1} is Greater than {n2}")
+elif n1<n2:
+    print(f"{n2} is Greater than {n1}")
+elif n1 == n2:
+    print("Both are equal")
+else:
+    print()
