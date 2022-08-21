1397f90c2e5f62d9c70efd415c85eec0849009b3
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-21 19:58:16 +0530

    Update Leep Year.py

--- Leep Year.py
+++ Leep Year.py
@@ -1,7 +1,7 @@
-m = int(input("Enter month in numeric format : "))
+m = int(input("Enter year in numeric format : "))
 if m%400 == 0 and m%100 == 0:
     print("%d is a Leap Year" %m)
-if m%4 == 0 and m%100 !=0:
+elif m%4 == 0 and m%100 !=0:
     print(f"{m} is a Leap Year")
 else:
     print("{0} is not a Leep Year".format(m))
