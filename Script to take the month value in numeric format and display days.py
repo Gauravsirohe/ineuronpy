6cb0d49f313ef29d02983712385e78c5e6576ae2
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-22 03:48:53 +0530

    Update Script to take the month value in numeric format and display days.py
    Two method mentioned.

--- Script to take the month value in numeric format and display days.py
+++ Script to take the month value in numeric format and display days.py
@@ -25,3 +25,21 @@ elif m ==12:
     print("December", "No of days : 31", sep="<")
 else:
     print()
+
+#Advance method
+m = [1,3,5,7,8,10,12]
+mm = [4,6,9,11]
+mmm = [2]
+n = int(input("Enter month in numeric : "))
+y = int(input("Enter year : "))
+if n in m:
+    print("31")
+elif n in mm:
+    print("30")
+elif n in mmm:
+    if y%4 ==0:
+        print("29")
+    else:
+        print("28")
+else:
+    print("Enter correct month value")
