4715fa6b08149df4af7395d58b077aaa7acb2f3d
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-22 03:20:30 +0530

    Add files via upload

---
+++ Script to take the month value in numeric format and display days.py
@@ -0,0 +1,27 @@
+m = int(input("Enter month in numeric format :"))
+if m ==1:
+    print("January", "No of days : 31", sep="<")
+elif m ==2:
+    print("Feburary", "No of days : 28", sep="<")
+elif m ==3:
+    print("March", "No of days : 31", sep="<")
+elif m ==4:
+    print("April", "No of days : 30", sep="<")
+elif m ==5:
+    print("May", "No of days : 31", sep="<")
+elif m ==6:
+    print("June", "No of days : 30", sep="<")
+elif m ==7:
+    print("July", "No of days : 31", sep="<")
+elif m ==8:
+    print("August", "No of days : 31", sep="<")
+elif m ==9:
+    print("September", "No of days : 30", sep="<")
+elif m ==10:
+    print("October", "No of days : 31", sep="<")
+elif m ==11:
+    print("November", "No of days : 30", sep="<")
+elif m ==12:
+    print("December", "No of days : 31", sep="<")
+else:
+    print()
