976db3a3a1fe40792efb753dd7df9fac703dfe8b
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-22 23:59:51 +0530

    Add files via upload

--- Python script to print first N natural numbers.py
+++ Python script to print first N natural numbers.py
@@ -1,6 +1,6 @@
 n = int(input("Enter a number here : "))
 i = 1
 while i <= n:
-    print(i, end = " ")
+    print(i+i, end = " ")
     i+=1
 print()
---
+++ Script to print first N even natural numbers in reverse order.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a number : "))
+i = n
+while i:
+    print(i+i, end = " ")
+    i = i -1
+print()
