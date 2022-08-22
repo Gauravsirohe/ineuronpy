f93ddb9c2575fe567dfb2217182efc33603696e4
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-22 22:52:36 +0530

    Add files via upload

---
+++ Print first N natural numbers in reverse order.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a number: "))
+i = n
+while i:            #every non zero value is false, so when it comes at zero by default condition will be false
+    print(i)
+    i-=1
+print()
