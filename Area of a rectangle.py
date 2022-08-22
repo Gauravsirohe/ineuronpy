8633e9f574923d6716e60845488c05d0270cc8a0
Gaurav Kumar <110766019+Gauravsirohe@users.noreply.github.com>
2022-08-22 13:50:16 +0530

    Add files via upload

---
+++ Area of a rectangle.py
@@ -0,0 +1,12 @@
+"""Area of Rectangle - Formula
+ The formula to find the area of a rectangle is A = l × w
+ l = length
+ w = width
+"""
+
+l = int(input("Enter length : "))
+w = int(input("Enter width : "))
+
+A = l*w
+
+print("Area of a rectangle is", A)
